8b6879168517a07104f9be2ebe0017d4d69a2d50
Kseniya <pen.cil@mail.ru>
2024-02-15 02:59:21 +0500

    fix: fix rendering of field

--- src/components/field/field.tsx
+++ src/components/field/field.tsx
@@ -1,25 +1,44 @@
 'use client';
-import { useAppSelector } from '@/redux/hooks';
-import Cell from '../cell';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import Cell, { CellType } from '../cell';
 import { createField } from '@/app/utils/createField';
+import { useEffect, useState } from 'react';
+import { setField } from '@/redux/slices/playGameSlice';
+
+export type FieldType = CellType[][];
 
 const Field = () => {
+  const field = useAppSelector((state) => state.playGame.field);
   const curMines: number = useAppSelector((state) => state.playGame.mines);
 
   const curRows: number = useAppSelector((state) => state.playGame.rows);
   const curCols: number = useAppSelector((state) => state.playGame.cols);
 
-  const currentField = createField(curRows, curCols, curMines);
+  const dispatch = useAppDispatch();
+
+  const [curField, setCurField] = useState<FieldType>(field);
+  console.log(curField);
+
+  useEffect(() => {
+    const { field } = createField(curRows, curCols, curMines);
+    setCurField(field);
+    dispatch(setField(field));
+  }, [curCols, curMines, curRows]);
+
+  useEffect(() => {
+    setCurField(field);
+  }, [field]);
 
   return (
     <div>
-      {currentField.map((row, idxRows) => (
-        <div className="flex m-auto" key={idxRows}>
-          {row.map((col, idxCols) => (
-            <Cell key={idxCols} value={col} />
-          ))}
-        </div>
-      ))}
+      {curField &&
+        curField.map((row, idxRows) => (
+          <div className="flex m-auto" key={idxRows}>
+            {row.map((col, idxCols) => (
+              <Cell key={idxCols} value={col} />
+            ))}
+          </div>
+        ))}
     </div>
   );
 };
